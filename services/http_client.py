bbaa97e691db0d122970c446572faad021241d6b
Pavel Bordukov <pawwel611@mail.ru>
2026-07-14 21:18:19 +0300

    Qa-diplom9

--- services/http_client.py
+++ services/http_client.py
@@ -4,6 +4,7 @@ import requests
 class HttpClient():
     def __init__(self, base_url: str) -> None:
         self.base_url = base_url
+
     @staticmethod
     def get_headers(token: str) -> dict:
         return {"Authorization": f"Bearer {token}"}
